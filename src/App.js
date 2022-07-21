0d53b640059aa701c729dde14c8d7bd5b3998403
flavia-dantas <flavia.dantas2@hotmail.com>
2022-07-20 22:24:54 -0300

    feat: Routes and components header, footer and home

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Header } from "./Components/Header";
+import { Footer } from "./Components/Footer";
+import { Home } from "./Components/Home";
+import { Login}  from "./Components/Login/Login";
 
 function App() {
   return (
     <div>
-      App React
+      <BrowserRouter>
+      <Header/>
+      <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="/login/*" element={<Login/>} />
+      </Routes>
+      <Footer/>
+      </BrowserRouter>
     </div>
   );
 }
