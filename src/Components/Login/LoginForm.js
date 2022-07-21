e164d2dc7bf2edf69c9ddf666ecae8d06819fed0
flavia-dantas <flavia.dantas2@hotmail.com>
2022-07-20 22:35:38 -0300

    feat: LoginForm request api

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -9,6 +9,26 @@ export const LoginForm = () => {
   console.log(username.value);
   console.log(password.value);
 
+  function handleSubmit(e) {
+    e.preventDefalt();
+
+    if (username.validate() && password.validate()) {
+      fetch("https://dogsapi.origamid.dev/json/jwt-auth/v1/token", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify()
+      })
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
+  }
 
   return (
     <section>
