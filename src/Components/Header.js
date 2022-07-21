c707dd4d7da98c80c03ba63757864d4a7a281123
flavia-dantas <flavia.dantas2@hotmail.com>
2022-07-21 19:06:04 -0300

    feat: link to navigate account

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,15 +1,28 @@
 import styles from "./Header.module.css";
 import { Link } from "react-router-dom";
 import { ReactComponent as Dogs } from "../Assets/dogs.svg";
+import { UserContext } from "../UserContext";
+import { useContext } from "react";
 
 export const Header = () => {
+  const { data, userLogout } = useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
         <Link className={styles.logo} to="/" aria-label="Dogs - Home">
-          <Dogs/>
+          <Dogs />
         </Link>
-        <Link className={styles.login} to="/login">Login / Criar</Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+            <button onChange={userLogout}>Sair</button>
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
