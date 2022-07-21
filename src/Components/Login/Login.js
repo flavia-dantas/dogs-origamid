f6af4151d22bdd72772df75bc90d9baca4aa5361
flavia-dantas <flavia.dantas2@hotmail.com>
2022-07-21 19:08:50 -0300

    refactor: login navigate to account

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -1,17 +1,22 @@
-import { Route, Routes } from "react-router-dom";
+import { useContext } from "react";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { UserContext } from "../../UserContext";
 import { LoginCreate } from "./LoginCreate";
 import { LoginForm } from "./LoginForm";
 import { LoginPasswordLost } from "./LoginPasswordLost";
 import { LoginPasswordReset } from "./LoginPasswordReset";
 
 export const Login = () => {
+  const { login } = useContext(UserContext);
+
+  if (login === true) return <Navigate to="/conta" />;
   return (
     <div>
       <Routes>
-        <Route path="/" element={<LoginForm/>}/>
-        <Route path="criar" element={<LoginCreate/>}/>
-        <Route path="recuperar" element={<LoginPasswordLost/>}/>
-        <Route path="senha" element={<LoginPasswordReset/>}/>
+        <Route path="/" element={<LoginForm />} />
+        <Route path="criar" element={<LoginCreate />} />
+        <Route path="recuperar" element={<LoginPasswordLost />} />
+        <Route path="senha" element={<LoginPasswordReset />} />
       </Routes>
     </div>
   );
