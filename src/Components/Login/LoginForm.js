28485dba872b1312540646c2ed0e21a4907ff8b0
flavia-dantas <flavia.dantas2@hotmail.com>
2022-07-21 19:02:09 -0300

    feat: userLogin in useContext

--- src/Components/Login/LoginForm.js
+++ src/Components/Login/LoginForm.js
@@ -1,44 +1,21 @@
-import { useEffect } from "react";
+import { useContext } from "react";
 import { Link } from "react-router-dom";
-import { GET_USER, TOKEN_POST } from "../../api";
+// import { GET_USER, TOKEN_POST } from "../../api";
 import { useForm } from "../../Hooks/useForm";
+import { UserContext } from "../../UserContext";
 import { Button } from "../Forms/Button";
 import { Input } from "../Forms/Input";
 
 export const LoginForm = () => {
   const username = useForm();
   const password = useForm();
-  // console.log(username.value);
-  // console.log(password.value);
-
-  useEffect(() => {
-    const token = window.localStorage.getItem("token");
-    if (token) {
-      getUser(token);
-    }
-  }, []);
-
-  async function getUser(token) {
-    const { url, options } = GET_USER(token);
-    const response = await fetch(url, options);
-    const json = await response.json();
-    console.log(json);
-  }
+  const { userLogin, error, loading } = useContext(UserContext);
 
   async function handleSubmit(e) {
     e.preventDefault();
 
     if (username.validate() && password.validate()) {
-      const { url, options } = TOKEN_POST({
-        username: username.value,
-        password: password.value,
-      });
-
-      const response = await fetch(url, options);
-      const json = await response.json();
-      console.log(json);
-      window.localStorage.setItem("token", json.token);
-      getUser(json.token);
+      userLogin(username.value, password.value);
     }
   }
 
@@ -48,7 +25,8 @@ export const LoginForm = () => {
       <form action="" onSubmit={handleSubmit}>
         <Input label="Usuário" type="text" name="username" {...username} />
         <Input label="Senha" type="password" name="password" {...password} />
-        <Button>Entrar</Button>
+        {loading ? (<Button disabled>Carregando...</Button>) : (<Button>Entrar</Button>)}
+        {error && <p>{error}</p>}
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
