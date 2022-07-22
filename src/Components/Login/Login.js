0f9fd1d1c953d3f8dc03f3df662888d3779e3355
flavia-dantas <flavia.dantas2@hotmail.com>
2022-07-21 23:29:34 -0300

    feat: style login component

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -5,19 +5,22 @@ import { LoginCreate } from "./LoginCreate";
 import { LoginForm } from "./LoginForm";
 import { LoginPasswordLost } from "./LoginPasswordLost";
 import { LoginPasswordReset } from "./LoginPasswordReset";
+import styles from "./Login.module.css";
 
 export const Login = () => {
   const { login } = useContext(UserContext);
 
   if (login === true) return <Navigate to="/conta" />;
   return (
-    <div>
-      <Routes>
-        <Route path="/" element={<LoginForm />} />
-        <Route path="criar" element={<LoginCreate />} />
-        <Route path="recuperar" element={<LoginPasswordLost />} />
-        <Route path="senha" element={<LoginPasswordReset />} />
-      </Routes>
-    </div>
+    <section className={styles.login}>
+      <div className={styles.forms}>
+        <Routes>
+          <Route path="/" element={<LoginForm />} />
+          <Route path="criar" element={<LoginCreate />} />
+          <Route path="recuperar" element={<LoginPasswordLost />} />
+          <Route path="senha" element={<LoginPasswordReset />} />
+        </Routes>
+      </div>
+    </section>
   );
 };
