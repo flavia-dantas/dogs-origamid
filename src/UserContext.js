075b9009c7718727a74b6b70fb194c27f4f49087
flavia-dantas <flavia.dantas2@hotmail.com>
2022-07-21 23:28:26 -0300

    refactor: navigate from dependencies and add await at const token

--- src/UserContext.js
+++ src/UserContext.js
@@ -19,7 +19,7 @@ export const UserStorage = ({ children }) => {
       window.localStorage.removeItem("token");
       navigate("/login");
     },
-    [navigate],
+    [],
   );
 
   async function getUser(token) {
@@ -36,8 +36,8 @@ export const UserStorage = ({ children }) => {
       setLoading(true);
       const { url, options } = TOKEN_POST({ username, password });
       const tokenRes = await fetch(url, options);
-      if (!tokenRes.ok) throw new Error(`Error: ${tokenRes.statusText}`);
-      const { token } = tokenRes.json();
+      if (!tokenRes.ok) throw new Error("Error: Usuário ou senha inválido.");
+      const { token } = await tokenRes.json();
       window.localStorage.setItem("token", token);
       await getUser(token);
       navigate("/conta");
