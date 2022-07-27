471347599c76bc4333cd3395f5ffb45946d79853
flavia-dantas <flavia.dantas2@hotmail.com>
2022-07-27 14:43:43 -0300

    feat:  PhotoComments component

--- src/Components/Photo/PhotoComments.js
+++ src/Components/Photo/PhotoComments.js
@@ -1,5 +1,26 @@
-export const PhotoComments = () => {
+import { useContext, useEffect, useRef, useState } from "react";
+import { UserContext } from "../../UserContext";
+import { PhotoCommentsForm } from "./PhotoCommentsForm";
+import styles from "./PhotoComments.module.css";
+
+export const PhotoComments = (props) => {
+  const [comments, setComments] = useState(() => props.comments);
+  const commentSection = useRef(null);
+  const { login } = useContext(UserContext);
+
+  useEffect(() => {
+    commentSection.current.scrollTop = commentSection.current.scrollHeight;
+  }, [comments]);
+
   return (
-    <div>PhotoComments</div>
+    <>
+      <ul ref={commentSection} className={styles.comments}>
+        {comments.map((comment) => (
+          <li key={comment.commnent_ID}>
+            <b>{comment.comment_author}:</b>
+            <span>{comment.comment_content}</span>
+          </li>))}
+      </ul>
+    </>
   );
 };
