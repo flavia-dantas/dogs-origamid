985f601e4a03a88147531560d098ebe0d58d403c
flavia-dantas <flavia.dantas2@hotmail.com>
2022-08-03 20:59:54 -0300

    rafactor: change classname view

--- src/Components/Photo/PhotoContent.js
+++ src/Components/Photo/PhotoContent.js
@@ -20,7 +20,7 @@ export const PhotoContent = ({ data, single }) => {
           <p className={styles.author}>
             {user.data && user.data.username === photo.author ? <PhotoDelete id={photo.id} /> : <Link to={`/perfil/${photo.author}`}>@{photo.author}</Link>}
 
-            <span className={styles.views}>{photo.acessos}</span>
+            <span className={styles.view}>{photo.acessos}</span>
           </p>
           <h1 className="title">
             <Link to={`/foto/${photo.id}`}>{photo.title}</Link>
